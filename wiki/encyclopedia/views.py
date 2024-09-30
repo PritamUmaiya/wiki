f69662480faff055e693120411e2c411590a691d
pritamjaiswal.me <pritamjaiswal.me@gmail.com>
2024-09-30 04:07:58 +0000

    update

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from markdown2 import Markdown
 
 from . import util
+
+
+def markdown2html(entry):
+    markdowner = Markdown()
+    return markdowner.convert(entry)
 
 
 def index(request):
@@ -8,3 +16,37 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    """If the requested entry"""
+    entry = util.get_entry(title)
+    if entry is not None:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": markdown2html(entry)
+        })
+    
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": "Requested entry does not exist!"
+        })
+
+
+def search(request):
+    if request.method == "POST":
+        query = request.POST["q"]
+        entries = util.list_entries()
+        results = []
+
+        if query in entries:
+            return HttpResponseRedirect(reverse("entry", title=query))
+
+        for entry in entries:
+            if query.lower() in entry.lower():
+                results.append(entry)
+
+        return render(request, "encyclopedia/search.html", {
+            "query": query,
+            "entries": results
+        })
