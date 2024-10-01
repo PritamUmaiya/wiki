7fd8102448d68e7a7e97ca8b2b2608630cdabc98
pritamjaiswal.me <pritamjaiswal.me@gmail.com>
2024-10-01 05:40:51 +0000

    adding search

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("wiki/<str:title>", views.entry, name="entry"),
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("search", views.search, name="search"),
+    path("wiki/<str:title>", views.entry, name="entry")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from markdown2 import Markdown
 
 from . import util
@@ -32,3 +34,22 @@ def entry(request, title):
         return render(request, "encyclopedia/apology.html", {
             "message": "Entry does not exists!"
         })
+
+
+def search(request):
+    """Return the search result"""
+    title = request.GET["q"]
+    entries = util.list_entries()
+
+    entries_list = []
+
+    for entry in entries:
+        if title == entry:
+            return HttpResponseRedirect(reverse("entry", args=[title]))
+        elif title.lower() in entry.lower():
+            entries_list.append(entry)
+
+    return render(request, "encyclopedia/search.html", {
+        "title": title,
+        "entries": entries_list
+    })
