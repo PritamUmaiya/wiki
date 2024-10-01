6cdf0aa63c5c37664ec51c917eb3638478f77701
pritamjaiswal.me <pritamjaiswal.me@gmail.com>
2024-10-01 05:17:22 +0000

    adding links

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("wiki/<str:title>", views.entry, name="entry"),
     path("", views.index, name="index")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
+
+
+def md2html(content):
+    markdowner = Markdown()
+    return markdowner.convert(content)
 
 
 def index(request):
@@ -8,3 +14,21 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    """Return the entry if existed"""
+    entry = util.get_entry(title)
+
+    if entry is not None:
+        # Convert entry to html
+        entry = md2html(entry)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "entry": entry
+        })
+    
+    else:
+        """Return the apology message"""
+        return render(request, "encyclopedia/apology.html", {
+            "message": "Entry does not exists!"
+        })
